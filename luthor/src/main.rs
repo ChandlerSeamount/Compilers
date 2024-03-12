c488a7546352002865106ebada79da98e629b511
Chandler Seamount <chandlerseamount@gmail.com>
2024-03-12 14:15:22 -0600

    Some bad tokens

--- luthor/src/main.rs
+++ luthor/src/main.rs
@@ -2,13 +2,15 @@ use std::env;
 use std::fs;
 use std::process;
 use std::io::{BufReader, BufRead};
+use std::collections::HashMap;
+use std::io::Write;
 
 #[derive(Clone)]
 struct TokenId {
     table: Vec<Vec<String>>,
     name: String,
     alt: String,
-    state: i32,
+    state: usize,
 }
 
 struct Token {
@@ -16,26 +18,30 @@ struct Token {
     id: TokenId,
     st_ln: i32,
     st_idx: i32,
-    idx: i32,
+    idx: usize,
     end_ln: i32,
     end_idx: i32,
 }
 
-fn make_alph(s:String) -> Vec<char> {
-    let mut v:Vec<char> = vec![];
+fn make_alph(s:String) -> HashMap<char, usize> {
+    let mut v = HashMap::new();
     let mut count = 0;
+    let mut other = 0;
     while s.as_bytes()[count] as char != '\n' {
         
         if s.as_bytes()[count] as char == 'x' {
             let temp = &s[count + 1..count + 3];
-            v.push(u8::from_str_radix(temp, 16).map(|n| n as char).expect("Bad value"));
+            v.insert(u8::from_str_radix(temp, 16).map(|n| n as char).expect("Bad value"), other);
             count += 3;
+            other += 1;
         } else if s.as_bytes()[count] as char == ' ' {
             count += 1;
         } else {
-            v.push(s.as_bytes()[count] as char);
+            v.insert(s.as_bytes()[count] as char, other);
             count += 1;
+            other += 1;
         }
+        
     }
     v
 }
@@ -94,11 +100,14 @@ fn main() {
             Ok(l) => l,
             Err(_) => process::exit(1),
         };
-        println!("{}", lin);
+        // println!("{}", lin);
         tables.push(make_token(lin));
     }
+    for (key, value) in &alph {
+        println!("{} / {}", key, value);
+    }
     tables.reverse();
-    let scource_string = fs::read_to_string(&args[2])?;
+    let scource_string = fs::read_to_string(&args[2]).expect("Failed to read file");
     let mut tokens:Vec<Token> = vec![];
     let mut start_line = 0;
     let mut start_idx = 0;
@@ -107,22 +116,23 @@ fn main() {
     let mut i = 0;
     let mut val_sub = "".to_string();
     let mut temp_tab = tables.to_vec();
-    let mut data_file = fs::OpenOptions::new().append(true).open(&args[3]).expect("cannot open file");
+    let mut out_file = fs::OpenOptions::new().append(true).create(true).open(&args[3]).expect("cannot open file");
     while i < scource_string.len() {
         let next = scource_string.chars().nth(i).unwrap();
         let mut next_tab:Vec<TokenId> = vec![];
         val_sub.push(next);
         
-        for v in temp_tab {
-            
-            if v.table[v.state][alph.find(next.to_string()) + 2] != "E".to_string() {
-                v.state = v.table[v.state][alph.find(next.to_string()) + 2].parse::<i32>().unwrap();
-                next_tab.push(v);
+        for mut v in temp_tab {
+            println!("{} / {}",v.state,next);
+            println!("{:?}", v.table);
+            if v.table[v.state][alph.get(&next).unwrap() + 2] != "E".to_string() {
+                v.state = v.table[v.state][alph.get(&next).unwrap() + 2].parse::<usize>().unwrap();
+                
                 if v.table[v.state][0] == "+".to_string() {
                     tokens.push(
                         Token {
-                            val: val_sub,
-                            id: v,
+                            val: val_sub.clone(),
+                            id: v.clone(),
                             st_ln: start_line,
                             st_idx: start_idx,
                             idx: i,
@@ -131,13 +141,15 @@ fn main() {
                         }
                     )
                 }
+                next_tab.push(v);
             }
         }
         
         temp_tab = next_tab.to_vec();
         next_tab.clear();
+        i += 1;
         if temp_tab.len() == 0 {
-            let Some(tok) = tokens.pop();
+            let Some(tok) = tokens.pop() else {panic!("Bad token")};
             let mut new_line = "".to_string();
             new_line.push_str(&tok.id.name);
             new_line.push(' ');
@@ -151,6 +163,16 @@ fn main() {
             new_line.push(' ');
             new_line.push_str(&tok.st_idx.to_string());
             new_line.push('\n');
+            out_file.write(new_line.as_bytes()).expect("Write failed");
+            start_line = tok.end_ln;
+            start_idx = tok.end_idx;
+            i = tok.idx;
+            val_sub = "".to_string();
+        }
+        cur_idx += 1;
+        if next == '\n' {
+            cur_line += 1;
+            cur_idx = 0;
         }
         
     }
